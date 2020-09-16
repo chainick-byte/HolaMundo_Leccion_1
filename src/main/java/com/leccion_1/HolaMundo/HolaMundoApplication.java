17802caba07f1aacfa0a9331580413e2c30d4a38
chainick-byte <igorrepyakh@gmail.com>
2020-09-16 14:31:26 +0100

    curso 2 primer ejercicio de SPRING, hola mundo

---
+++ src/main/java/com/leccion_1/HolaMundo/HolaMundoApplication.java
@@ -0,0 +1,13 @@
+package com.leccion_1.HolaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leccion_1/HolaMundo/HolaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leccion_1.HolaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
